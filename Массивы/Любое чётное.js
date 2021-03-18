22fb3b714f1b0f50262d01accf6dd6d61eb8505a
Куцерубова Влада <80004001+vlada98K@users.noreply.github.com>
2021-03-18 13:09:40 +0500

    Create Любое чётное.js

---
+++ Массивы/Любое чётное.js
@@ -0,0 +1,20 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let lines = [];
+
+rl.on('line', function(line){
+    lines.push(line);
+});
+
+rl.on('close', () => {
+   let result = lines[0];
+    console.log(result
+               .split(' ')
+               .map(a => +a)
+               .filter(a => a%2 == 0)[0]);
+});
